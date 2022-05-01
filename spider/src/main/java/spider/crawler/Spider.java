260b19cfc21c1e280b7e403348049f32c6ffa484
Paul Stitt <pstitt1970@gmail.com>
2022-05-01 12:21:58 +0100

    Refactored Spider class to make more readable

--- spider/src/main/java/spider/crawler/Spider.java
+++ spider/src/main/java/spider/crawler/Spider.java
@@ -2,62 +2,48 @@ package spider.crawler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import spider.crawler.internal.CrawledPageTracker;
 import spider.crawler.internal.Link;
 import spider.crawler.internal.PageParser;
 import spider.crawler.internal.ParsedPage;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Consumer;
 
 @Slf4j
 @RequiredArgsConstructor
 public class Spider implements Crawler {
   private final PageParser parser;
-  private String baseURL;
-  private final CrawledPageTracker tracker = new CrawledPageTracker();
+  private final SpiderContext context = new SpiderContext();
 
   @Override
   public void crawl(String pageURL, PageVisitor pageVisitor) {
-    Link link = toLink(pageURL);
-    setBaseURLIfNotSet(link);
-    var canonicalURL = link.canonicalURL();
+    final var link = toLink(pageURL);
 
-    if (tracker.alreadyVisited(canonicalURL)) {
+    if (context.alreadyVisited(link)) {
       return;
     }
-    tracker.markVisited(canonicalURL);
 
-    pageVisitor.visit(canonicalURL);
+    context.markLinkAsVisited(link);
+    pageVisitor.visit(link.canonicalURL());
+    parsePageAndProcess(link, pageVisitor);
+  }
 
+  private void parsePageAndProcess(Link link, PageVisitor pageVisitor) {
+    final Consumer<ParsedPage> processLinks = parsedPage -> processLinks(parsedPage, pageVisitor);
+    parsePage(link.canonicalURL()).ifPresent(processLinks);
+  }
+
+  private Optional<ParsedPage> parsePage(String url) {
     try {
-      var parsedPage = parser.parse(canonicalURL);
-      processLinks(parsedPage, pageVisitor);
+      var parsedPage = parser.parse(url);
+      return Optional.of(parsedPage);
     } catch (IOException e) {
-      log.warn("Unable to parse html for page: {}", pageURL, e);
-    }
-  }
-
-  private Link toLink(String pageURL) {
-    if (baseURL == null) {
-      return Link.fromURL(pageURL);
-    } else {
-      try {
-        return Link.fromBaseURLAndPage(baseURL, pageURL);
-      } catch (MalformedURLException e) {
-        log.warn("Unable to parse link URL: {}", pageURL, e);
-        return Link.fromURL(pageURL);
-      }
-    }
-  }
-
-  private void setBaseURLIfNotSet(Link link) {
-    if (baseURL == null) {
-      try {
-        baseURL = link.getBaseURL();
-      } catch (MalformedURLException e) {
-        log.warn("Unable to get base URL for: {}", link.canonicalURL(), e);
-      }
+      log.warn("Unable to parse html for page: {}", url, e);
+      return Optional.empty();
     }
   }
 
@@ -68,12 +54,49 @@ public class Spider implements Crawler {
   private void processLink(String page, String linkURL, PageVisitor pageVisitor) {
     pageVisitor.link(page, linkURL);
     Link link = toLink(linkURL);
-    if (link.isInternal(baseURL)) {
+    if (link.isInternal(context.baseURL)) {
       followLink(linkURL, pageVisitor);
     }
   }
 
   private void followLink(String link, PageVisitor pageVisitor) {
     crawl(link, pageVisitor);
+  }
+
+  private Link toLink(String pageURL) {
+    if (context.baseURL == null) {
+      return Link.fromURL(pageURL);
+    } else {
+      try {
+        return Link.fromBaseURLAndPage(context.baseURL, pageURL);
+      } catch (MalformedURLException e) {
+        log.warn("Unable to parse link URL: {}", pageURL, e);
+        return Link.fromURL(pageURL);
+      }
+    }
+  }
+
+  private static class SpiderContext {
+    private String baseURL;
+    private final Set<String> visitedPages = new HashSet<>();
+
+    private void markLinkAsVisited(Link link) {
+      setBaseURLIfNotSet(link);
+      visitedPages.add(link.canonicalURL());
+    }
+
+    private void setBaseURLIfNotSet(Link link) {
+      if (baseURL == null) {
+        try {
+          baseURL = link.getBaseURL();
+        } catch (MalformedURLException e) {
+          log.warn("Unable to get base URL for: {}", link.canonicalURL(), e);
+        }
+      }
+    }
+
+    private boolean alreadyVisited(Link link) {
+      return visitedPages.contains(link.canonicalURL());
+    }
   }
 }
